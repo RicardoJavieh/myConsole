2549e7afed407ac097c813423497996a5f2ce705
rjavier <rjavier@better.com.mx>
2021-03-16 20:42:55 -0600

    First Commit

---
+++ myConsole/reader.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using Newtonsoft.Json;
+
+namespace myConsole {
+    class poolCommands {
+        dynamic jsonFile = JsonConvert.DeserializeObject("");
+    }
+    class ioConsole {
+        Regex baseCommand = new Regex(@"^(command1|command2|command3)([\s]|$)[\w ./:'+-]*");
+        private string commandRaw;
+        public int setCommand(string auxCommand) {
+            commandRaw = auxCommand;
+            return 0;
+        }
+        public string getCommand(string auxCommand) {
+            return auxCommand;
+        }
+        private int processCommand() {
+            
+            return 0;            
+        }
+
+    }
+}
